7fad566a600a2d03fbb852b60ca8af4314eb5100
Pedro <pedrogabrielcruzlima@gmail.com>
2024-02-24 15:07:44 -0300

    new migrations for postgresql

---
+++ Vendaval.Infrastructure/Data/Migrations/20240224171508_AddPhoneNumber.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vendaval.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPhoneNumber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Users",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Users");
+        }
+    }
+}
