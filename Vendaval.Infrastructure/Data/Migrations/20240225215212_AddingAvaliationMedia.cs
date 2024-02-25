083f7e8707ac1318d69cb5553672da56d9943a6a
Pedro <pedrogabrielcruzlima@gmail.com>
2024-02-25 18:53:06 -0300

    new migrations

---
+++ Vendaval.Infrastructure/Data/Migrations/20240225215212_AddingAvaliationMedia.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Vendaval.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingAvaliationMedia : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "ProductAvaliations",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "NOW()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "GETDATE()");
+
+            migrationBuilder.CreateTable(
+                name: "AvaliationMedia",
+                columns: table => new
+                {
+                    ProductAvaliationId = table.Column<int>(type: "integer", nullable: false),
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Url = table.Column<string>(type: "text", nullable: false),
+                    MediaType = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AvaliationMedia", x => new { x.ProductAvaliationId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_AvaliationMedia_ProductAvaliations_ProductAvaliationId",
+                        column: x => x.ProductAvaliationId,
+                        principalTable: "ProductAvaliations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AvaliationMedia");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "ProductAvaliations",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValueSql: "GETDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldDefaultValueSql: "NOW()");
+        }
+    }
+}
