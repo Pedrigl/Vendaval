5d5595478ca02a4f51da007cbda0b74ab0364322
Pedro <pedrogabrielcruzlima@gmail.com>
2024-02-26 13:18:20 -0300

    Login service

--- vendaval.client/src/app/login/login.service.ts
+++ vendaval.client/src/app/login/login.service.ts
@@ -3,30 +3,31 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment'; 
 import { Login, LoginResponse } from './login';
 import { User } from './user';
+import { AuthorizedHttpClient } from '../shared/common/authorized-httpclient';
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private authClient: AuthorizedHttpClient) { }
 
   login(loginModel: Login) {
-    this.httpClient.post<LoginResponse>(environment.apiUrl + 'User/login', loginModel);
+    return this.httpClient.post<LoginResponse>(environment.apiUrl + 'User/login', loginModel);
   }
 
   register(user: User) {
-    this.httpClient.post(environment.apiUrl + 'User/register', user);
+    return this.httpClient.post(environment.apiUrl + 'User/register', user);
   }
 
-  put(user: User) {
-    this.httpClient.put(environment.apiUrl + 'User/put', user);
+  putUser(user: User) {
+    return this.authClient.put(environment.apiUrl + 'User/put', user);
   }
 
-  patch(user: User) {
-    this.httpClient.patch(environment.apiUrl + 'User/patch', user);
+  patchUser(user: User) {
+    return this.authClient.patch(environment.apiUrl + 'User/patch', user);
   }
 
-  delete(id: Number) {
-    this.httpClient.delete(environment.apiUrl + `User/delete?id=${id}`);
+  deleteUser(id: Number) {
+    return this.authClient.delete(environment.apiUrl + `User/delete?id=${id}`);
   }
 }
