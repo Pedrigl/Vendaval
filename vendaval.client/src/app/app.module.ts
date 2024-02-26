4de604ef2959729881f884b406377e0e25dc46de
Pedro <pedrogabrielcruzlima@gmail.com>
2024-02-25 22:47:45 -0300

    Adicionando bootstrap no front

--- vendaval.client/src/app/app.module.ts
+++ vendaval.client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -11,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
