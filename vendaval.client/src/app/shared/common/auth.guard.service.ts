99d6e6d92165e7f7a7eaf69383ffd4e873e67419
Pedro <pedrogabrielcruzlima@gmail.com>
2024-03-04 17:22:12 -0300

    final authguard

--- vendaval.client/src/app/shared/common/auth.guard.service.ts
+++ vendaval.client/src/app/shared/common/auth.guard.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { ActivatedRoute, ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs/internal/Observable';
+import { lastValueFrom, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate{
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> {
+    return this.authService.getUser.pipe(
+      map(user => {
+        if (user == null) {
+          return this.router.createUrlTree(['/login'], { queryParams: { reason: "you are not logged in" } });
+        }
+
+        const roles = route.data['roles'] as Array<string>;
+        if (roles && !roles.includes(user.userType.toString())) {
+          return this.router.createUrlTree(['/login'], { queryParams: { reason: `you don't have the permissions to access ${state.url}` } });
+        }
+
+        return true;
+      })
+    );
+  }
 }
