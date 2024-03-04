1f2e36ffe4267de546a9e830cb1a3c8422bb4e1f
Pedro <pedrogabrielcruzlima@gmail.com>
2024-03-04 17:00:37 -0300

    fixed error

--- vendaval.client/src/app/login/login.component.ts
+++ vendaval.client/src/app/login/login.component.ts
@@ -64,7 +64,7 @@ export class LoginComponent implements OnInit{
         this.loginError = errorParse.message;
       }
       else {
-        this.loginError = error;
+        this.loginError = error.error;
       }
 
       return
