5e31798992b1096df6c5e614bc88749a1416119c
Pedro <pedrogabrielcruzlima@gmail.com>
2024-03-05 00:22:59 -0300

    fixing user type display

--- vendaval.client/src/app/admin/users/users.component.ts
+++ vendaval.client/src/app/admin/users/users.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { User } from '../../login/user';
 import { LoginService } from '../../login/login.service';
 import { ApiResponse } from '../../shared/common/interfaces/apiResponse';
+import { UserType } from '../../login/user-type';
 
 @Component({
   selector: 'app-users',
@@ -10,7 +11,8 @@ import { ApiResponse } from '../../shared/common/interfaces/apiResponse';
 })
 export class UsersComponent {
     users!: ApiResponse<User[]>;
-
+    userType = UserType;
+    
     constructor(private loginService: LoginService) {
       loginService.getUsers().subscribe(response => {
         this.users = response;
