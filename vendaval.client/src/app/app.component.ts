61e7a63dc3d44230828520e68b9f38d1371f4e05
Pedro <pedrogabrielcruzlima@gmail.com>
2024-03-05 03:57:23 -0300

    new activities

--- vendaval.client/src/app/app.component.ts
+++ vendaval.client/src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-
+//TODO: ADD A GLOBAL ERROR HANDLER
 export class AppComponent implements OnInit {
 
   constructor() {}
