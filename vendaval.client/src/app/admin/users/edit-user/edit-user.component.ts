5b5c867ebd3a2eb0e1b159a7bfb0e0f0b283c238
Pedro <pedrogabrielcruzlima@gmail.com>
2024-03-06 00:51:48 -0300

    just dont allow birthdate change lol

--- vendaval.client/src/app/admin/users/edit-user/edit-user.component.ts
+++ vendaval.client/src/app/admin/users/edit-user/edit-user.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { LoginService } from '../../../login/login.service';
 import { User } from '../../../login/user';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ApiResponse } from '../../../shared/common/interfaces/apiResponse';
 import { Observable } from 'rxjs/internal/Observable';
 import { lastValueFrom } from 'rxjs/internal/lastValueFrom';
@@ -16,23 +16,28 @@ export class EditUserComponent{
     hasError = false;
     error = '';
 
-    constructor(private route: ActivatedRoute,private loginService: LoginService){
+    constructor(private route: ActivatedRoute, private router: Router,private loginService: LoginService){
         
         this.loginService.getUser(this.route.snapshot.queryParams['id']).subscribe(response => {
-            this.user = response.data;
+          this.user = response.data;
         })
     }
     async saveUser() {
-        try {
-            var req = await lastValueFrom(this.loginService.putUser(this.user));
-            this.user = req.user;
-            this.hasError = false;
-          this.error = req.message;
+      try {
+        this.user.userType = Number(this.user.userType);
+        var req = await lastValueFrom(this.loginService.putUser(this.user));
+        this.user = req.user;
+        this.hasError = false;
+        this.error = req.message;
+        this.router.navigate(['/admin/users']);
         }
 
-        catch (error:any) {
-            this.hasError = true;
-            this.error = error.message;
+        catch (error: any) {
+          this.hasError = true;
+          if (typeof (error) === 'string') {
+            this.error = error;
+          }
+            this.error = error;
         }
         
     }
