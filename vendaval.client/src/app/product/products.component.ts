ce658e2598af0818d01f0fa5f538897d5f8c7451
Pedro <pedrogabrielcruzlima@gmail.com>
2024-03-10 03:26:19 -0300

    added product type and price filter

--- vendaval.client/src/app/product/products.component.ts
+++ vendaval.client/src/app/product/products.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { LoadingService } from '../shared/common/loading.service';
 import { ProductService } from './product.service';
 import { Product } from './product';
+import { ProductType } from './product-type';
 
 @Component({
   selector: 'app-products',
@@ -11,13 +12,50 @@ import { Product } from './product';
 })
 export class ProductsComponent {
   products!: Product[];
+  productTypes = ProductType;
+  productTypesNames = Object.keys(ProductType).filter(key => isNaN(Number(key)));
+  productTypesValues = Object.values(ProductType).filter(value => !isNaN(Number(value)));
+  fiteredProductTypes!: ProductType;
+  filteredProducts!: Product[];
+  nameFilter: string = '';
+  initialPriceFilter: number = 0;
+  maxPriceFilter: number = 0;
+
+
   constructor(private router: Router, private loadingService: LoadingService, private productService: ProductService) {
     this.loadingService.isLoading.next(true);
     productService.getAllProducts().subscribe(response => {
       this.products = response.data;
+      this.filteredProducts = [...this.products];
       this.loadingService.isLoading.next(false);
     });
   }
 
+  applyNameFilter() {
+    if (this.nameFilter) {
+      this.filteredProducts = this.products.filter(p => p.name.toLowerCase().includes(this.nameFilter.toLowerCase()));
+    }
+    else {
+      this.filteredProducts = [...this.products];
+    }
+  }
+
+  applyPriceFilter() {
+    if (this.initialPriceFilter > 0 && this.maxPriceFilter > 0) {
+      this.filteredProducts = this.products.filter(p => p.price >= this.initialPriceFilter && p.price <= this.maxPriceFilter);
+    }
+    else {
+      this.filteredProducts = [...this.products];
+    }
+  }
+
+  applyCategoryFilter() {
+    if (Number(this.fiteredProductTypes) != -1) {
+      this.filteredProducts = this.products.filter(p => p.category == this.fiteredProductTypes);
+    }
+    else {
+      this.filteredProducts = [...this.products];
+    }
+  }
 
 }
