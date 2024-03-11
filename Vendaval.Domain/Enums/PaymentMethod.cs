4e31184da94b142f2c98f2829810feb36fea8b69
Pedro <pedrogabrielcruzlima@gmail.com>
2024-03-11 01:16:27 -0300

    added payment method enum

---
+++ Vendaval.Domain/Enums/PaymentMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vendaval.Domain.Enums
+{
+    public enum PaymentMethod
+    {
+        Pix = 1,
+        CreditCard = 2,
+        DebitCard = 3
+    }
+}
