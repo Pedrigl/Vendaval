083c520e7899e59ca4c5c5582a1517c00c258fc3
Pedro <pedrogabrielcruzlima@gmail.com>
2024-03-11 13:39:36 -0300

    added addresses to user controller

--- Vendaval.Api/Controllers/UserController.cs
+++ Vendaval.Api/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using Vendaval.Application.ViewModels;
 using Vendaval.Application.ValueObjects;
 using Vendaval.Application.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Identity.Client;
 
 namespace Vendaval.Api.Controllers
 {
@@ -12,10 +13,11 @@ namespace Vendaval.Api.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserViewModelSerivce _userViewModelService;
-
-        public UserController(IUserViewModelSerivce userViewModelService)
+        private readonly IUserAddressViewModelService _userAddressViewModelService;
+        public UserController(IUserViewModelSerivce userViewModelService, IUserAddressViewModelService userAddressViewModelService)
         {
             _userViewModelService = userViewModelService;
+            _userAddressViewModelService = userAddressViewModelService;
         }
 
         [HttpPost("login")]
@@ -137,6 +139,82 @@ namespace Vendaval.Api.Controllers
             try
             {
                 var result = await _userViewModelService.DeleteLogin(id);
+
+                if(!result.Success)
+                    return BadRequest(result);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [Authorize]
+        [HttpGet("address/getByUserId")]
+        public async Task<IActionResult> GetAddressByUserId(int userId)
+        {
+            try
+            {
+                var result = await _userAddressViewModelService.GetUserAddressByUserId(userId);
+
+                if(!result.Success)
+                    return BadRequest(result);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [Authorize]
+        [HttpPost("address/add")]
+        public async Task<IActionResult> AddAddress([FromBody] UserAddressViewModel userAddressViewModel)
+        {
+            try
+            {
+                var result = await _userAddressViewModelService.AddAddress(userAddressViewModel);
+
+                if(!result.Success)
+                    return BadRequest(result);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [Authorize]
+        [HttpPut("address/update")]
+        public async Task<IActionResult> UpdateAddress([FromBody] UserAddressViewModel userAddressViewModel)
+        {
+            try
+            {
+                var result = await _userAddressViewModelService.UpdateAddress(userAddressViewModel);
+
+                if(!result.Success)
+                    return BadRequest(result);
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [Authorize]
+        [HttpDelete("address/delete")]
+        public async Task<IActionResult> DeleteAddress([FromBody] UserAddressViewModel userAddressViewModel)
+        {
+            try
+            {
+                var result = await _userAddressViewModelService.DeleteAddress(userAddressViewModel);
 
                 if(!result.Success)
                     return BadRequest(result);
