5bc77e5594f6189b410bff5e82ed494540923726
Pedro <pedrogabrielcruzlima@gmail.com>
2024-03-14 15:15:04 -0300

    conversation model

--- vendaval.client/src/app/chat/conversation.ts
+++ vendaval.client/src/app/chat/conversation.ts
@@ -1,7 +1,8 @@
+import { BaseModel } from "../shared/common/interfaces/BaseModel";
 import { ChatUser } from "./chatuser";
 import { Message } from "./message";
 
-export interface Conversation {
-  sender: ChatUser;
+export interface Conversation extends BaseModel{
+  participants: ChatUser[];
   messages: Message[];
 }
