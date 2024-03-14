633c2cef6bc2661da515c4a8dbb1fcd821b77d8d
Pedro <pedrogabrielcruzlima@gmail.com>
2024-03-14 15:16:09 -0300

    conversation model on the backend

---
+++ Vendaval.Application/ViewModels/ConversationViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vendaval.Application.ViewModels
+{
+    public class ConversationViewModel : BaseModelViewModel
+    {
+        public List<ChatUserViewModel> Participants { get; set; }
+        public List<MessageViewModel> Messages { get; set; }
+    }
+}
