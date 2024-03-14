078e8265349ae079551e8f65f13897f07744fbdc
Pedro <pedrogabrielcruzlima@gmail.com>
2024-03-14 16:57:12 -0300

    Chat User repository

---
+++ Vendaval.Infrastructure/Data/Repositories/EFRepositories/Interfaces/IChatUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vendaval.Domain.Entities;
+
+namespace Vendaval.Infrastructure.Data.Repositories.EFRepositories.Interfaces
+{
+    public interface IChatUserRepository
+    {
+        Task<ChatUser> GetByIdAsync(int id);
+        Task<List<ChatUser>> GetAllAsync();
+        IEnumerable<ChatUser> GetWhere(Func<ChatUser, bool> predicate);
+        Task<ChatUser> AddAsync(ChatUser entity);
+        void Update(int entityId, ChatUser entity);
+        void Delete(ChatUser entity);
+    }
+}
