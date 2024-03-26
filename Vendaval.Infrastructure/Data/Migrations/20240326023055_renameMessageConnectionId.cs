df9d1a6acec3327bf7cbd34c15dd5dcb7b5175d7
Pedro <pedrogabrielcruzlima@gmail.com>
2024-03-25 23:33:27 -0300

    renaming messages connectionid

---
+++ Vendaval.Infrastructure/Data/Migrations/20240326023055_renameMessageConnectionId.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Vendaval.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class renameMessageConnectionId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SenderId",
+                table: "Message",
+                newName: "SenderConnectionId");
+
+            migrationBuilder.RenameColumn(
+                name: "ReceiverId",
+                table: "Message",
+                newName: "ReceiverConnectionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "SenderConnectionId",
+                table: "Message",
+                newName: "SenderId");
+
+            migrationBuilder.RenameColumn(
+                name: "ReceiverConnectionId",
+                table: "Message",
+                newName: "ReceiverId");
+        }
+    }
+}
